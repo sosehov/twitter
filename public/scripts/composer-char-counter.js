d7633d680d5638b9225984a826b0f88e4495c57c
Soseh <sosehv@gmail.com>
2025-01-27 21:53:33 -0500

    Adds logic to find the counter relative to the textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,14 +5,17 @@ $(document).ready(function() {
     const currentLength = $(this).val().length;
     const remainingLength = maxLength - currentLength;
 
+    // Find the counter relative to the textarea
+    const $counter = $(this).closest('.new-tweet').find('.counter');
+
     // Update the counter
-    $('.counter').text(remainingLength);
+    $counter.text(remainingLength);
 
     // Check if remaining length is negative
     if (remainingLength < 0) {
-      $('.counter').addClass('over-limit');
+      $counter.addClass('over-limit');
     } else {
-      $('.counter').removeClass('over-limit');
+      $counter.removeClass('over-limit');
     }
     });
   });
