a1fa9b9b6601f21f9131660200608a47f9f61a9e
Soseh <sosehv@gmail.com>
2025-01-29 22:54:06 -0500

    Adds renderTweets function and fake data to test it

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,20 +6,41 @@
 
 $(document).ready(()=> {
 
-  const tweetData = {
+  // Fake data taken from initial-tweets.json
+const data = [
+  {
     "user": {
       "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
     "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
     "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+  const renderTweets = function(tweets) {
+    for(const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet); 
+    }
   }
 
-  const createTweetElement = function(tweetData) {
-    const tweet = $(`
+  const createTweetElement = function(tweet) {
+    let tweet = $(`
       <article class="tweet">
         <header class="tweet-header">
           <br/>
@@ -43,9 +64,6 @@ $(document).ready(()=> {
     return $tweet;
   };
 
-  const $tweet = createTweetElement(tweetData);
-
-  console.log($tweet);
-  $('#tweets-container').append($tweet); 
+  renderTweets(data);
 
 });
