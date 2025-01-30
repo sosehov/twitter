9630af44d569643cb59230f1af827aa1c567ba70
Soseh <sosehv@gmail.com>
2025-01-30 12:46:06 -0500

    Adds tweet as a parameter to createTweetElement function and removes fake data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,38 +6,12 @@
 
 $(document).ready(()=> {
 
-  // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-  const createTweetElement = function() {
+  const createTweetElement = function(tweet) {
     let tweet = $(`
       <article class="tweet">
         <header class="tweet-header">
           <br/>
-          <img src="${tweet.user.avatars} alt="User's profile picture">
+          <img src="${tweet.user.avatars}" alt="User's profile picture">
           <h3>${tweet.user.name}</h3>
           <h3>${tweet.user.handle}</h3>
         </header>
@@ -54,7 +28,7 @@ const data = [
         </article>
     `);
 
-    return $tweet;
+    return tweet;
   };
 
   // Grab the twitter container from the webpage
