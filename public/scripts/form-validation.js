2eb5d7d902dfd976128a17e5f401e9bcf648ac6d
Soseh <sosehv@gmail.com>
2025-01-31 15:03:29 -0500

    Adds logic to show an alert when tweet is empty of longer than 140 chars

--- public/scripts/form-validation.js
+++ public/scripts/form-validation.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
-  // Register the 'input' event on the textarea
-  $('.new-tweet textarea').on('submit', function() {
+
+  $('.tweet-form').on('submit', function(event) {
     const maxLength = 140;
-    const currentLength = $(this).value().length;
-    if (currentLength === null || currentLength === '') {
+    const tweetText = $('.new-tweet textarea').val();
+    const currentLength = tweetText.length;
+
+    // Clear any previous error messages
+    $('.error-message').remove();
+
+    if (!tweetText) {
       alert('Tweet can not be empty!');
+      event.preventDefault();
     } else if (currentLength > maxLength) {
       alert('Tweet text can not exceed 140 characters!');
+      event.preventDefault();
       }
   });
 });
