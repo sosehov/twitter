1bf9e69b6e983c5be9e1f6970b1000d40cdd4609
Soseh <sosehv@gmail.com>
2025-02-05 21:23:05 -0500

    Removes old comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 $(document).ready(()=> {
   // Handle the toggle functionality
   $('.toggle-tweet-form').on('click', function() {
